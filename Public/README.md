8b31b0d426d3a5e680a1cab7bfca3fccd5739769
Desmond <desmond.yan.lai@gmail.com>
2022-02-23 09:03:18 -0500

    hid yelp api key

---
+++ Public/README.md
@@ -0,0 +1,1 @@
+Restaurant locator app made with mapbox and yelp api. Find the nearest restaurant with reviews, price range, and more!
--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require('express');
 const fetch = require('cross-fetch');
 const { response } = require('express');
 const res = require('express/lib/response');
-const app = express();
+require('dotenv').config();
 
-app.listen(3000, () => console.log('listening at 3000'));
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening at server ${port}`)
+});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 
 app.get('/map/:latlon', async (request, response) => {
   const latlon = request.params.latlon.split(',');
   const lat = latlon[0];
   const lon = latlon[1];
 
-  const API_key = 'NeeK12jNxlEAng-IzrJeyS2vV7XbhyXADQmZK8_rLgVb8EzTfy3S-1fJZTYbeM-HpflLGC8gKwbOczWDgxHn_ul-sT2LDYRqBYn4_0AtxBMxeu8PGQqx3YHYJosRXHYx';
+  const API_key = process.env.YELP_API_KEY;
   const api_url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${lat}&longitude=${lon}`;
 
   const options = {
